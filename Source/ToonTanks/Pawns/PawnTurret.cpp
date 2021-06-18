f04953548a86742662699b84f3ca925a4874969d
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-06-18 19:20:56 +0100

    Created a new class to handle the turret only functionality & attached it to the turret blueprint, also implemented a player distance check and a firing timer for the projectile

--- Source/ToonTanks/Pawns/PawnTurret.cpp
+++ Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -2,4 +2,40 @@
 
 
 #include "PawnTurret.h"
+#include "PawnTank.h"
+#include "Kismet/GameplayStatics.h"
 
+// Called when the game starts or when spawned
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorldTimerManager().SetTimer(FireRateTimer, this, &APawnTurret::CheckFireCondition, FireRate, true);
+
+	PlayerPawn = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+void APawnTurret::CheckFireCondition()
+{
+    if(!PlayerPawn) return;
+
+	if(DistanceToPlayer() <= FireRange)
+	{
+		// Pew Pew
+		UE_LOG(LogTemp, Warning, TEXT("PewPew"));
+	}
+
+}
+
+float APawnTurret::DistanceToPlayer() const
+{
+	if(!PlayerPawn) return 0.0f;
+	
+	return FVector::Dist(PlayerPawn->GetActorLocation(), GetActorLocation());
+}
+
+// Called every frame
+void APawnTurret::Tick(const float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
--- Source/ToonTanks/Pawns/PawnTurret.h
+++ Source/ToonTanks/Pawns/PawnTurret.h
@@ -6,12 +6,32 @@
 #include "PawnBase.h"
 #include "PawnTurret.generated.h"
 
-/**
- * 
- */
+class APawnTank;
+
 UCLASS()
 class TOONTANKS_API APawnTurret : public APawnBase
 {
 	GENERATED_BODY()
+
+	private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRate = 2.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRange = 500.0f;
+
+	FTimerHandle FireRateTimer;
+	APawnTank* PlayerPawn;
 	
+	void CheckFireCondition();
+	float DistanceToPlayer() const;
+	
+	public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+		
 };
